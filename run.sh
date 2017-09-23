bf771fb1eb5f14b8b7b330e57a3ff0d49f6ad3c8
Ibuki Hideaki <ibuki90@gmail.com>
2017-09-24 00:10:14 +0900

    起動スクリプトを変更

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-docker run -d  -p 25566:25565 -v ./world:/data/world -v ./logs:/data/logs -e EULA=TRUE --name minecraft ./
+docker build -t csr-minecraft ./
+docker run -d -u 1000:1000 -p 25566:25565 -v world:/data/world -v logs:/data/logs -e EULA=TRUE --name minecraft csr-minecraft
