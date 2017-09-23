a036792a7279a7b13d08c06b1ad2c2ebcb688f6a
Ibuki Hideaki <ibuki90@gmail.com>
2017-09-24 01:37:53 +0900

    volumeの位置を変更

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ mkdir config
 mkdir mods
 mkdir plugins
 docker build -t csr-minecraft ./
-docker run -d  -p 25565:25565 -v data:/data -v config:/config -v mods:/mods -v plugins:/plugins -e EULA=TRUE --name minecraft csr-minecraft
+docker run -d  -p 25565:25565 -v /home/minecraft/csr-minecraft/data:/data -v /home/minecraft/csr-minecraft/config:/config -v /home/minecraft/csr-minecraft/mods:/mods -v /home/minecraft/csr-minecraft/plugins:/plugins -e EULA=TRUE --name minecraft csr-minecraft
